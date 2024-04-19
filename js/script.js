e1868d3f1394d326cdba1b0c312c705ccc35ee38
JanMajer86 <majer.jan.1986@gmail.com>
2024-04-19 10:16:18 +0200

    pagination javascript functionality implemented

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var items = document.querySelectorAll(".item");
+// CAROUSEL FUNCTIONALITY
 
 items.forEach((it) => {
   it.onclick = () => {
@@ -17,3 +18,53 @@ items.forEach((it) => {
     }
   };
 });
+
+// PAGINATION FUNCTIONALITY attemp 01
+const pageLinks = document.querySelectorAll(".page-link");
+const buttonLeft = document.querySelector(".btn--left");
+const buttonRight = document.querySelector(".btn--right");
+
+let page = 0;
+const maxLength = pageLinks.length - 1;
+
+console.log("page: " + page);
+changePaginationNumber(page);
+
+buttonLeft.addEventListener("click", function () {
+  arrowButtonClick(-1);
+});
+
+buttonRight.addEventListener("click", function () {
+  arrowButtonClick(1);
+});
+
+for (let i = 0; i < pageLinks.length; i++) {
+  pageLinks[i].id = i;
+}
+
+function getId(btn) {
+  console.log("button id: " + btn.id);
+  page = parseInt(btn.id);
+  console.log("page: " + page);
+
+  changePaginationNumber(page);
+}
+
+function arrowButtonClick(direction) {
+  page += direction;
+
+  if (page < 0) page = 0;
+  if (page > maxLength) page = maxLength;
+
+  console.log("page: " + page);
+
+  changePaginationNumber(page);
+}
+
+function changePaginationNumber(page) {
+  pageLinks.forEach((pageLink) => {
+    pageLink.classList.remove("page-link--current");
+  });
+  pageLinks[page].classList.add("page-link--current");
+  console.log("current page: " + page);
+}
